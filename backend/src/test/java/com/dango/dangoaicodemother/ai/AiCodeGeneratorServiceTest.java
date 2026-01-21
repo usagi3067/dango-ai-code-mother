12a822dc78a8aa3348f77aa9962ab00759eb5c7d
dango <1227894812@qq.com>
2026-01-21 14:18:27 +0800

    接入ai， 实现单 HTML、 多文件生成 prompt

---
+++ backend/src/test/java/com/dango/dangoaicodemother/ai/AiCodeGeneratorServiceTest.java
@@ -0,0 +1,34 @@
+package com.dango.dangoaicodemother.ai;
+
+import com.dango.dangoaicodemother.ai.model.HtmlCodeResult;
+import com.dango.dangoaicodemother.ai.model.MultiFileCodeResult;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author dango
+ * @description
+ * @date
+ */
+@SpringBootTest
+class AiCodeGeneratorServiceTest {
+
+    @Resource
+    private AiCodeGeneratorService aiCodeGeneratorService;
+
+    @Test
+    void generateHtmlCode() {
+        HtmlCodeResult result = aiCodeGeneratorService.generateHtmlCode("做个dango的工作记录小工具");
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    void generateMultiFileCode() {
+        MultiFileCodeResult multiFileCode = aiCodeGeneratorService.generateMultiFileCode("做个dango的留言板");
+        Assertions.assertNotNull(multiFileCode);
+    }
+}
---
+++ backend/src/test/java/com/dango/dangoaicodemother/core/AiCodeGeneratorFacadeTest.java
@@ -0,0 +1,29 @@
+package com.dango.dangoaicodemother.core;
+
+import com.dango.dangoaicodemother.model.enums.CodeGenTypeEnum;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author dango
+ * @description
+ * @date
+ */
+@SpringBootTest
+class AiCodeGeneratorFacadeTest {
+
+    @Resource
+    private AiCodeGeneratorFacade aiCodeGeneratorFacade;
+
+    @Test
+    void generateAndSaveCode() {
+        File file = aiCodeGeneratorFacade.generateAndSaveCode("任务记录网站", CodeGenTypeEnum.MULTI_FILE);
+        Assertions.assertNotNull(file);
+    }
+}
