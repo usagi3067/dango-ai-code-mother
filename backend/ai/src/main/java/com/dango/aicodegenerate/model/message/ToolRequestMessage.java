26e8ae2715696d630eeb9de2185f7656086ff54b
dango <1227894812@qq.com>
2026-02-14 17:25:38 +0800

    feat: ToolRequestMessage 新增 filePath 和 action 字段

--- backend/ai/src/main/java/com/dango/aicodegenerate/model/message/ToolRequestMessage.java
+++ backend/ai/src/main/java/com/dango/aicodegenerate/model/message/ToolRequestMessage.java
@@ -19,10 +19,32 @@ public class ToolRequestMessage extends StreamMessage {
 
     private String arguments;
 
+    /**
+     * 文件路径（流式工具专用）
+     */
+    private String filePath;
+
+    /**
+     * 操作类型 write/modify
+     */
+    private String action;
+
     public ToolRequestMessage(ToolExecutionRequest toolExecutionRequest) {
         super(StreamMessageTypeEnum.TOOL_REQUEST.getValue());
         this.id = toolExecutionRequest.id();
         this.name = toolExecutionRequest.name();
         this.arguments = toolExecutionRequest.arguments();
+    }
+
+    /**
+     * 流式工具专用构造方法
+     */
+    public ToolRequestMessage(String id, String name, String filePath, String action, String arguments) {
+        super(StreamMessageTypeEnum.TOOL_REQUEST.getValue());
+        this.id = id;
+        this.name = name;
+        this.filePath = filePath;
+        this.action = action;
+        this.arguments = arguments;
     }
 }
